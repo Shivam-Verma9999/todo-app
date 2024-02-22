e98a193e278b670642b57b6e8caa7cabc0a53e46
Shivam Verma <sv805388@gmail.com>
2024-02-22 21:20:28 +0530

    render deploy setup

--- src/components/LoginEntryPoint.js
+++ src/components/LoginEntryPoint.js
@@ -1,91 +1,90 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import './LoginEntryPoint.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./LoginEntryPoint.css";
 
-import { config, API_URL } from '../utility/Config';
+import { config, API_URL } from "../utility/Config";
 
-import logoImg from '../images/todo-icon.png';
+import logoImg from "../images/todo-icon.png";
 export default class LoginEntryPoint extends Component {
+  onSubmit = (e) => {
+    e.preventDefault();
 
-    onSubmit = (e) => {
-        e.preventDefault();
+    let errorDiv = document.querySelector(".loginError");
 
-        let errorDiv = document.querySelector('.loginError');
+    if (errorDiv.classList.contains("loginErrorActive")) {
+      errorDiv.classList.remove("loginErrorActive");
+    }
+    errorDiv.innerHTML = "";
+    // alert('dfsd');
+    console.log(`Calling onSubmit in login form`);
+    // console.log(e.target);
+    let username = e.target.username.value.trim();
+    let url = `${API_URL}/auth/signIn`;
+    // console.log(username);
+    let password = e.target.password.value.trim();
 
-        if (errorDiv.classList.contains('loginErrorActive')) {
-            errorDiv.classList.remove('loginErrorActive');
+    let body = {
+      username: username,
+      password: encodeURIComponent(password),
+    };
+
+    console.log(body.username);
+    axios
+      .post(url, body, config)
+      .then((result) => {
+        console.log("then", result);
+        console.log("dd", result.status);
+        console.log("dd", result.statusText);
+        if (result.status !== 200) {
+          // alert('wrong username or password');
+          errorDiv.classList.add("loginErrorActive");
+          errorDiv.innerHTML = "Wrong username or password";
+        } else {
+          this.props.login();
         }
-        errorDiv.innerHTML = "";
-        // alert('dfsd');
-        console.log(`Calling onSubmit in login form`);
-        // console.log(e.target);
-        let username = e.target.username.value.trim();
-        let url = `${API_URL}/auth/signIn`;
-        // console.log(username);
-        let password = e.target.password.value.trim();
+      })
+      .catch((err) => {
+        // Do somthing
+        errorDiv.classList.add("loginErrorActive");
+        if (err.toString().indexOf("401") != -1) {
+          errorDiv.innerHTML = "Wrong username or password";
+        } else {
+          errorDiv.innerHTML = err;
+        }
+      });
+  };
 
-
-
-        let body = {
-            username: (username),
-            password: encodeURIComponent(password)
-        };
-
-
-        console.log(body.username);
-        axios.post(url, body, config)
-            .then((result) => {
-                console.log('then', result);
-                console.log("dd", result.status);
-                console.log("dd", result.statusText);
-                if (result.status !== 200) {
-                    // alert('wrong username or password');
-                    errorDiv.classList.add('loginErrorActive');
-                    errorDiv.innerHTML = 'Wrong username or password';
-
-                } else {
-                    this.props.login();
-                }
-            })
-            .catch((err) => {
-                // Do somthing
-                // console.log('errr', err);
-                // alert(err);
-            })
-
-    }
-
-    //TODO: show email varification message
-    render() {
-        return <div className="container">
-            <div className="form">
-                <div className="welcome">
-                    <div className="logo-img">
-                        <img src={logoImg} height="100%" alt="person" />
-                    </div>
-                    <h1>Welcome,</h1>
-                    <p>Lets plan your day</p>
-                </div>
-                <div className="loginError">
-                </div>
-                <div className="form-container">
-                    <form onSubmit={this.onSubmit}>
-                        <label for="username">Email</label>
-                        <br />
-                        <input id="username" name="username" type="email" />
-                        <br />
-                        <label for="password">Password</label>
-                        <br />
-                        <input id="password" name="password" type="password" />
-                        <br />
-                        <input type="submit" value="Login" />
-                    </form>
-                </div>
-                <div className="message">
-                    Dont have an account?, <a href="./signup.html">Signup</a>
-                </div>
+  //TODO: show email varification message
+  render() {
+    return (
+      <div className="container">
+        <div className="form">
+          <div className="welcome">
+            <div className="logo-img">
+              <img src={logoImg} height="100%" alt="person" />
             </div>
+            <h1>Welcome,</h1>
+            <p>Lets plan your day</p>
+          </div>
+          <div className="loginError"></div>
+          <div className="form-container">
+            <form onSubmit={this.onSubmit}>
+              <label for="username">Email</label>
+              <br />
+              <input id="username" name="username" type="email" />
+              <br />
+              <label for="password">Password</label>
+              <br />
+              <input id="password" name="password" type="password" />
+              <br />
+              <input type="submit" value="Login" />
+            </form>
+          </div>
+          <div className="message">
+            Dont have an account?, <a href="./signup.html">Signup</a>
+          </div>
         </div>
-    }
-
+      </div>
+    );
+  }
 }
--- src/utility/Config.js
+++ src/utility/Config.js
@@ -1,13 +1,16 @@
 const config = {
-    // headers: {
-    //     'Content-Type': 'application/x-www-form-urlencoded'
-    // },
-    withCredentials: true
-}
+  // headers: {
+  //     'Content-Type': 'application/x-www-form-urlencoded'
+  // },
+  withCredentials: true,
+};
 //for local testing
-// const API_URL = 'http://localhost:3000/api';
+const API_URL = "./api";
 
-//hosted api link
-const API_URL = 'https://todo-app-server-8zs8.onrender.com/api';
+// moving inside api static folder
+// const API_URL = "./api";
+
+// //hosted api link
+// const API_URL = 'https://todo-app-server-8zs8.onrender.com/api';
 
 export { config, API_URL };
